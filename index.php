699d72ef3034ff00cb42610d173f18df0f321bec
Admirl44 <36560341+admiral44@users.noreply.github.com>
2022-02-12 20:26:45 +0530

    component section add

--- index.php
+++ index.php
@@ -20,48 +20,10 @@
 <body>
 
   <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="navbar">
-    <div class="container d-flex align-items-center justify-content-around">
-      <div class="row rowsl align-items-center no-gutters">
-
-        <div class="col-md-4 col-lg-4 d-flex justify-content-between">
-          <a class="navbar-brand" href="#">Navbar</a>
-
-          <button class="navbar-toggler hmenu" type="button" data-toggle="collapse"
-            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        </div>
-
-        <div class="collapse navbar-collapse col-md-8 col-lg-8" id="navbarSupportedContent">
-
-          <div class="col-md-6 col-lg-6 col-sm-12 gubbuC">
-            <a class="navbar-brand" href="#">Home</a>
-            <a class="navbar-brand" href="#aboutUsSection">AboutUS</a>
-            <a class="navbar-brand" href="#">ContactUS</a>
-          </div>
-
-          <div class="col-md-6 col-lg-6 col-sm-12 gubbuC">
-            <a class="navbar-brand" href="#">Facebook</a>
-            <a class="navbar-brand" href="#">Whatsapp</a>
-            <a class="navbar-brand" href="#">Linked-in</a>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  </nav>
+  <?php include("component/navbarComp.php"); ?>
 
   <!-- Hero Page Image -->
-  <div class="container-fluid heroC">
-    <div class="text-center">
-      <h1>Dunguu</h1>
-      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum minima voluptate voluptas <br> maxime qui
-        nesciunt exercitationem eaque tenetur cumque. Quisquam minus autem hic voluptatum, neque debitis asperiores
-        dolorum eligendi? Ea?</p>
-    </div>
-  </div>
+  <?php include("component/section-1.php"); ?>
 
   <!-- Card Section -->
   <div class="container cardSection" id="aboutUsSection">
