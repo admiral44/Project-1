cb3d02952c96e15338f0c832d4f6285689f52d11
Admirl44 <36560341+admiral44@users.noreply.github.com>
2022-02-25 01:23:59 +0530

    Change Message to >> contact me

--- component/section-5.php
+++ component/section-5.php
@@ -1,6 +1,6 @@
 <div class="container p-5">
     <div class="col-12 sec5img">
-        <h1>Contact</h1>
+        <h1>Contact Me</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci quibusdam ducimus ipsam, <br>omnis harum repudiandae nisi qui dolore nemo voluptatum quis sed numquam et laborum blanditiis. Minima nobis commodi ratione.</p>
         <button class="btn btn-outline-success">Click here</button>
     </div>
